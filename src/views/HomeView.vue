444de6f17e22a4268f0afadbffb74e6955699fe0
kevin ponce <kevin.ponce@ieesa.edu.hn>
2024-09-12 16:45:29 -0600

    mensaje

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <h2>Esta pagina y about son solo rutas de ejemplo revisar Productos</h2>
+    <h4>No pidio otras rutas o diseño en especifico </h4>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
